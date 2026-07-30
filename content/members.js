a52d4679cbdf89f8961a0a5665ba1a064c54b372
Tanya Warrier <warriertanya@gmail.com>
2026-07-30 16:53:33 +0800

    feat: add member profiles

---
+++ content/members.js
@@ -0,0 +1,23 @@
+[
+    {
+        "id": "vishrutiranjan",
+        "name": "Vishruti Ranjan",
+        "title": "Chair",
+        "subtitle": "PhD student in Computer Science",
+        "photo": "assets/images/members/vishrutiranjan.jpg",
+    },
+    {
+        "id": "tanyawarrier",
+        "name": "Tanya Warrier",
+        "title": "Vice Chair",
+        "subtitle": "Masters student in Artificial Intelligence",
+        "photo": "assets/images/members/tanyawarrier.jpg",
+    },
+    {
+        "id": "shreyagangopadhyay",
+        "name": "Shreya Gangopadhyay",
+        "title": "Membership Chair",
+        "subtitle": "PhD student in Computer Science",
+        "photo": "assets/images/members/shreyagangopadhyay.jpg",
+    },
+]
